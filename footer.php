e4d0413551cb09fc131e444ccf1bc8187b47d4ac
Brandon Brotsky <Brandon@brotskydesigns.com>
2018-05-10 13:22:01 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Infinite_Group
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+				<?php
+				printf( esc_html__( 'Website Developed by %1$s.', 'infinite_group' ), '<a href="https://www.cadre.haus">Cadre</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<?php if (site_url() === 'http://localhost:8888/infinite') { ?>
+	<script src="http://localhost:35729/livereload.js"></script>
+<?php } ?>
+
+</body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const jshint = require('gulp-jshint');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const livereload = require('gulp-livereload');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const util = require('gulp-util');
+
+gulp.task('sass', function () {
+    gulp.src('sass/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer, cssnano]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(''))
+    // .pipe(livereload())
+});
+
+gulp.task('js', function () {
+    gulp.src([
+        'dev/**/*.js'
+    ])
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(jshint.reporter('fail'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('theme.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('js'))
+    // .pipe(livereload())
+});
+
+gulp.task('life-watch', function () {
+    livereload.listen();
+    gulp.watch('sass/**/*.scss', ['sass']).on('change', livereload.changed);
+    gulp.watch('dev/**/*.js', ['js']).on('change', livereload.changed);
+    gulp.watch('**/*.php').on('change', function (file) {
+        livereload.changed(file.path);
+        util.log(util.colors.yellow('File changed' + ' (' + file.path + ')'));
+    });
+});
+
+gulp.task('default', ['sass', 'js']);
+gulp.task('watch', ['sass', 'js', 'life-watch']);
