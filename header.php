c6c8dbb218ca183a49067c354989588c8a0ba3d6
Brandon Brotsky <Brandon@brotskydesigns.com>
2018-05-11 09:21:45 -0700

    progress on header

--- header.php
+++ header.php
@@ -24,24 +24,9 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'infinite_group' ); ?></a>
 
-	<header id="masthead" class="site-header">
+	<header id="masthead" class="site-header fixed-top">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$infinite_group_description = get_bloginfo( 'description', 'display' );
-			if ( $infinite_group_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $infinite_group_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			<img id="logo" src="<?php echo get_template_directory_uri() ?>/images/infinite-logo.svg" />
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
