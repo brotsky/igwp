8d6069e1a5dfb14c07d9cc0e5b83445cb4461883
Brandon Brotsky <Brandon@brotskydesigns.com>
2018-05-16 15:48:41 -0700

    first part of the logo animation

--- dev/theme.js
+++ dev/theme.js
@@ -1,10 +1,43 @@
 $(document).ready(function() {
   new Glide('.glide').mount();
 
-  var preloader = $('#preloader');
+  var preloader = $('#preloader'),
+      preloaderSvg = $('#preloader-svg');
+  
   if(preloader.length > 0) {
 
     TweenMax.set(preloader, { opacity: 1 });
+    TweenMax.set('#icon-dot', { y: -15 });
+
+    $(preloaderSvg).addClass('show');
+    var containerWidth = $('#preloader .container').width(),
+        containerHeight = $('#preloader .container').height(),
+        logoBoundingBox = $('#logo-wrapper')[0].getBBox(),
+        logoWidth = logoBoundingBox.width,
+        logoHeight = logoBoundingBox.height,
+        logoXOffset = (containerWidth - logoWidth) / 2,
+        logoYOffset = (containerHeight - logoHeight) / 2,
+        logoScale = logoWidth < containerWidth * 0.9 ? 1 : containerWidth / logoWidth * 0.9;
+    $(preloaderSvg).attr('width', containerWidth + 'px');
+    $(preloaderSvg).attr('height', containerHeight + 'px');
+    $(preloaderSvg).attr('viewBox', '0 0 ' + containerWidth + ' ' + containerHeight);
+    $(preloaderSvg).attr('style', 'enable-background:new 0 0 ' + containerWidth + ' ' + containerHeight);
+
+    TweenMax.set('#logo-wrapper', {
+      x: logoXOffset,
+      y: logoYOffset,
+      scale: logoScale,
+      transformOrigin:"50% 50%"
+    });
+
+    TweenMax.set('#icon-dot', {
+      y: -logoYOffset - logoHeight,
+    });
+
+    TweenMax.to('#icon-dot', 1, {
+      y: 0,
+      ease: Bounce.easeOut,
+    });
 
     TweenMax.to(preloader, 0.5, {opacity: 0, delay: 3, onComplete: function() {
       $(preloader).remove();
