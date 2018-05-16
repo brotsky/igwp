b8b063075d52ae4c47d00319837c431483ee9e32
Brandon Brotsky <Brandon@brotskydesigns.com>
2018-05-16 13:57:51 -0700

    starting the preloader

--- home-template.php
+++ home-template.php
@@ -9,5 +9,6 @@ get_header();
 <main id="main" class="site-main">
     <?php get_template_part("template-parts/home-page-carousel"); ?>
 </main><!-- #main -->
+<?php get_template_part("template-parts/preloader"); ?>
 <?php
 get_footer();
