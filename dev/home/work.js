647efd2f4043edcb9ca3716f856267a0d7d6a450
Brandon Brotsky <Brandon@brotskydesigns.com>
2018-05-17 13:42:05 -0700

    adding theme.min.js to gitignore

--- dev/home/work.js
+++ dev/home/work.js
@@ -1,11 +1,15 @@
 jQuery(function ($) {
-
-  var homeWorks = $('#home-works');
-
   var resizeHomeWorkContainers = function() {
-    if(homeWorks.length == 0) return;
 
+    var homeWorks = $('#home-works');
+
+    if(homeWorks.length === 0) return;
     var containers = $(homeWorks).find('.fold-col');
+    if(containers.length === 0) return;
+
+    for(var i = 0 ; i < containers.length ; i+=1) {
+      console.log(containers[i]);
+    }
     
     // we need to resize the containers so they cover the screen
   };
