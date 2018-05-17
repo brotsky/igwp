1e9d0948973dba322fa1cb740df5803e31846676
Brandon Brotsky <Brandon@brotskydesigns.com>
2018-05-17 14:09:40 -0700

    fixed work sizing issue

--- dev/home/work.js
+++ dev/home/work.js
@@ -1,17 +1,29 @@
 jQuery(function ($) {
   var resizeHomeWorkContainers = function() {
+    setTimeout(function(){
+      var homeWorks = $('#home-works');
 
-    var homeWorks = $('#home-works');
+      if(homeWorks.length === 0) return;
+      var containers = $(homeWorks).find('.fold-col');
+      if(containers.length === 0) return;
 
-    if(homeWorks.length === 0) return;
-    var containers = $(homeWorks).find('.fold-col');
-    if(containers.length === 0) return;
+      for(var i = 0 ; i < containers.length ; i+=1) {
+        var container = containers[i];
 
-    for(var i = 0 ; i < containers.length ; i+=1) {
-      console.log(containers[i]);
-    }
-    
-    // we need to resize the containers so they cover the screen
+        // reset height
+        $(container).find('.text-content-row').attr('style', '');
+
+        var columnHeight = $(window).height() - 100,
+            imageHeight = $(container).find('.image-content-row img').height(),
+            newTextHeight = columnHeight - imageHeight;
+
+        // setting height
+        $(container).find('.text-content-row').css({
+          height: newTextHeight
+        });
+        
+      }
+    }, 50);
   };
 
   $(document).ready(resizeHomeWorkContainers);
