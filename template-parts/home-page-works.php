af66c72778398fce11b5a47f2f8473b8d614a68d
Brandon Brotsky <Brandon@brotskydesigns.com>
2018-05-17 11:29:47 -0700

    started works section

--- template-parts/home-page-works.php
+++ template-parts/home-page-works.php
@@ -1,3 +1,20 @@
 <section class="fold" id="home-works" data-section-name="works">
-  <h3>Works</h3>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="fold-col col d-flex align-content-stretch flex-wrap">
+        <div class="row text-content-row brown-background">
+          <div class="col">
+            <p>Infinite Group specializes in custom contract furniture design production and manufacturing. We offer state of the art equipment in glass, acrylic and metal work. Infinite Group has a full service woodworking shop with endless materials and techniques to support the diverse needs of our clients.</p>
+          </div>
+        </div>
+        <div class="row text-content-row brown-background">
+          <div class="col">
+            <p>Infinite Group specializes in custom contract furniture design production and manufacturing. We offer state of the art equipment in glass, acrylic and metal work. Infinite Group has a full service woodworking shop with endless materials and techniques to support the diverse needs of our clients.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+      </div>
+    </div>
+  </div>
 </section>
