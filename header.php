d73782866e62d892330b31df9c6e29472cc6c7cb
margeshpatel <margeshpatel@gmail.com>
2018-05-19 02:50:15 +0530

    Mobile Menu without any animation

--- header.php
+++ header.php
@@ -30,7 +30,11 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'infinite_group' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<div class="mobile-menu-bar"></div>
+				<div class="mobile-menu-bar"></div>
+				<div class="mobile-menu-bar"></div>
+			</button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
