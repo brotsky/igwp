b9e131ef85d2e5d58b5088ca387cc8bdd010aeaa
Margesh Patel <margeshpatel@gmail.com>
2018-06-07 01:46:52 +0530

    Home Page contact section in place

--- template-parts/home-page-contact.php
+++ template-parts/home-page-contact.php
@@ -1,3 +1,22 @@
 <section class="fold" id="home-contact" data-section-name="contact">
-  <h3>Contact</h3>
+	<div class="container">
+    	<div class="row">
+			<?php if ( have_rows( 'contact_fold_content' ) ): ?>
+				<?php while ( have_rows( 'contact_fold_content' ) ) : the_row(); ?>
+					<?php if ( get_row_layout() == 'one_column_content' ) : ?>
+						<div class="col-sm-8 offset-sm-2">
+							<?php the_sub_field( 'content' ); ?>
+						</div>
+					<?php elseif ( get_row_layout() == 'two_column_content' ) : ?>
+						<div class="col-sm-6">
+							<?php the_sub_field( 'left_column_content' ); ?>
+						</div>
+						<div class="col-sm-6">
+							<?php the_sub_field( 'right_column_content' ); ?>
+						</div>
+					<?php endif; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
 </section>
